dd0cd224e2263d6ca161b97f5798b42df84f7ff3
Ezequiel Videla <ezequielev@live.com.ar>
2024-03-04 22:06:58 -0300

    new file:   prepare/Learning02/Job.cs 	modified:   prepare/Learning02/Program.cs 	new file:   prepare/Learning02/Resume.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+class Program {
+    static void Main(string[] args){
+        Job job1 = new Job();
+        Job job2 = new();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2016;
+        job1._endYear = 2019;
+        
+        job2._company = "Microsoft";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ezequiel Videla";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResumeDetails();
     }
 }
